a74f176d3bed570602a40a5dfbe5dde01ea1021d
Mr-jk03 <giangcuong0603@gmail.com>
2024-10-14 22:31:17 +0700

    update user login

--- src/components/Header/User/UserForm/UserForm.jsx
+++ src/components/Header/User/UserForm/UserForm.jsx
@@ -1,62 +1,40 @@
-import React, { useEffect, useState } from 'react';
+
+import { useState } from 'react';
 import './UserForm.css';
 
 const UserForm = () => {
-    const [signUpInvisible, setSignUpInvisible] = useState(true);
-    const [signinInvisible, setSignInInvisible] = useState(true);
 
-    const handleShowSignupForm = () => {
-        setSignUpInvisible(false);
-        setSignInInvisible(false);
-    };
-
-    const checkSizeWindow = () => {
-        if (window.innerWidth < 431) {
-            setSignInInvisible(true); 
-            setSignUpInvisible(true); 
-        } else {
-            setSignInInvisible(true); 
-            setSignUpInvisible(false); 
-        }
-    };
-
-    useEffect(() => {
-        checkSizeWindow();
-
-        window.addEventListener('resize', checkSizeWindow);
-
-        return () => {
-            window.removeEventListener('resize', checkSizeWindow);
-        };
-    }, []);
+    const [isSignUp, setIsSignUp] = useState(true);
+    const toggleForm = () => {
+        setIsSignUp(!isSignUp);
+      };
 
     return (
         <div className='main-userform'>
             <div className="container main-form-us">
                 <div className="row">
-                    {/* Hiển thị form đăng nhập */}
-                    {signinInvisible && (
-                        <div className="col-xl-6 col-lg-6 col-md-6 sign-cha">
-                            <div className='text-us'>
-                                <span>ĐĂNG NHẬP</span>
+                {isSignUp ? (
+                    <div className="col-xl-6 col-lg-6 col-md-6 sign-cha">
+                        <div className='text-us'>
+                            <span>ĐĂNG NHẬP</span>
+                        </div>
+                        <div className='input-info-us'>
+                            <label>Tên đăng nhập</label>
+                            <input type="text" />
+                            <label>Mật khẩu</label>
+                            <input type="text" />
+                            <div className='row button-sign-forgot'>
+                                <button className='col-xl-6 col-lg-6 col-md-6 col-sm-6'>Đăng nhập</button>
+                                <button className='col-xl-6 col-lg-6 col-md-6 col-sm-6'>Quên mật khẩu ?</button>
                             </div>
-                            <div className='input-info-us'>
-                                <label>Tên đăng nhập</label>
-                                <input type="text" />
-                                <label>Mật khẩu</label>
-                                <input type="text" />
-                                <div className='row button-sign-forgot'>
-                                    <button className='col-xl-6 col-lg-6 col-md-6 col-sm-6'>Đăng nhập</button>
-                                    <button className='col-xl-6 col-lg-6 col-md-6 col-sm-6'>Quên mật khẩu ?</button>
-                                </div>
-                            </div>
+                        </div>
                             <div className='br'>
                                 <div></div>
                                 <span>Hoặc</span>
                                 <div></div>
                             </div>
                             <div className='button-forgot'>
-                                <button onClick={handleShowSignupForm}>Đăng ký</button>
+                                <button onClick={toggleForm}>Đăng ký</button>
                             </div>
                             <div className='button-face-google'>
                                 <button>Facebook</button>
@@ -68,10 +46,7 @@ const UserForm = () => {
                                 Hoặc Inbox: giangcuong0603@gmail.com
                             </span>
                         </div>
-                    )}
-
-                    {/* Hiển thị form đăng ký */}
-                    {!signUpInvisible && (
+                        ) : (
                         <div className="col-xl-6 col-lg-6 col-md-6 sign-cha sign-up-cha">
                             <div className='text-us'>
                                 <span>ĐĂNG KÝ</span>
